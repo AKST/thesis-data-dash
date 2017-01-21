0e99cd4bc28c48d8d294fec10aeabef6ccc873e6
Angus Thomsen <aksthomsen@gmail.com>
2017-01-21 18:09:30 +1100

    added packages page

--- src/init/router.js
+++ src/init/router.js
@@ -1,11 +1,23 @@
 import Index from 'src/routes/index'
+import PackageRoot from 'src/routes/package'
 import NotFound from 'src/routes/not-found'
 
 export default {
   mode: 'history',
   base: __dirname,
   routes: [
-    { path: '/', component: Index },
-    { path: '/*', component: NotFound }
+    {
+      path: '/',
+      component: Index
+    },
+    {
+      path: '/package',
+      component: PackageRoot,
+      children: []
+    },
+    {
+      path: '/*',
+      component: NotFound
+    }
   ]
 }
